79b3049906775dea7f6338daa49bd63943768b50
yurii-yu <yusheng.email@gmail.com>
2014-07-10 21:34:42 +0800

    rails new interview

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Interview::Application.config.secret_key_base = '8f0989517497bd23ecda69e4cb7f2b48b79f0dbb14d8959b0889131e794886182300e1418021c438c216618c6a6e8a8fe6a950efaa0050389fd81c990ecf0255'
