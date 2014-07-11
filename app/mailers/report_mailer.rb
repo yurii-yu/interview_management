467f32b0f7c0b08c309def0f73ee80f7b43aa4af
yurii-yu <yusheng.email@gmail.com>
2014-07-11 13:17:46 +0800

    defined send function

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,3 +1,10 @@
 class ReportMailer < ActionMailer::Base
   default from: "yurii.yu@oceanwing.com"
+
+  def welcome_email(report)
+    @report = report
+    #mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: 'hr@oceanwing.com', subject: "面试反馈 #{@report.name} #{@report.position}")
+  end
+
 end
