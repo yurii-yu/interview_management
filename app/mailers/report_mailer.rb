0e2795167b86544ee69a8d440a399587e669ceca
yurii-yu <yusheng.email@gmail.com>
2014-07-11 23:28:03 +0800

    send to all participator's email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 class ReportMailer < ActionMailer::Base
-  #default from: 'peter.liu@oceanwing.com'
   default from: '"Yurii" <' + ENV['OCW_NAME'] + ">"
 
   def send_report(report)
     @report = report
-    mail(to: ENV['RESUME_HR'],  subject: "面试反馈 #{@report.name} #{@report.position}")
+    #mail(to: ENV['RESUME_HR'],  subject: "面试反馈 #{@report.name} #{@report.position}")
+    mail(to: Participator.all.map{|x| x.email}.join(","),  subject: "面试反馈 #{@report.name} #{@report.position}")
   end
 
 end
