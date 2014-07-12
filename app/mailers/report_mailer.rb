87608fef51e29e0f3b5fe7a0b0f6d170764d4a43
yurii-yu <yusheng.email@gmail.com>
2014-07-12 16:09:08 +0800

    another way of selecting recipient's email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 class ReportMailer < ActionMailer::Base
-  default from: '"Yurii" <' + ENV['OCW_NAME'] + ">"
 
   def send_report(report, user)
     @report = report
     @user = user
-    mail(to: (Participator.all.select{|x| x.email != @user.email}.map{|x| x.email}.<<@user.email).join(","), 
+    mail(to: (Participator.where("email <> '#{@user.email}'").map{|x| x.email}.<<@user.email),#.join(","), 
     from: "\"#{@user.username}\" <#{@user.email}>",
     subject: "面试反馈 #{@report.name} #{@report.position}")
   end
