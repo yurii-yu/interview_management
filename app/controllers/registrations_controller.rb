e56262805d966167b884f2da919b002c195d7d1c
yurii-yu <yusheng.email@gmail.com>
2014-07-12 13:41:06 +0800

    save email password safely without changed to blank

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
-   new_params = params.require(:user).permit(:username)
+   new_params = params.require(:user).permit(:username, :email_password)
+
+   if new_params['email_password'].empty? 
+     new_params.delete('email_password')
+   end
 
    @user = User.find(current_user.id)
    if @user.update_without_password(new_params)
      set_flash_message :notice, :updated
-     sign_in @user, :bypass => true
-     redirect_to after_update_path_for(@user)
+     #sign_in @user, :bypass => true
+     render "edit"
+     #redirect_to after_update_path_for(@user)
    else
      render "edit"
    end
