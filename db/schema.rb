8646c05b2dd273af636169307dfc3a1485c1c7bb
yurii-yu <yusheng.email@gmail.com>
2014-07-12 10:54:10 +0800

    rails generate migration add_username_to_users username:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711142554) do
+ActiveRecord::Schema.define(version: 20140712025153) do
 
   create_table "participators", force: true do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140711142554) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
